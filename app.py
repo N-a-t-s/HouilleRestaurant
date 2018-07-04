e104023cb76ed6f1d19a475f331efa09b8880c9c
Stanislav Stefinyn <stanislav.stefinyn@epitech.eu>
2018-07-04 15:56:24 +0200

    advancement in home.html

--- app.py
+++ app.py
@@ -8,13 +8,30 @@ from flask import render_template
 app = Flask(__name__)
 app.debug = True
 
-@app.route("/")
-def hello():
-    return "Hello World!"
-
 @app.route("/home")
 def home():
 	return render_template('home.html')
+
+@app.route("/contacts")
+def contacts():
+	return render_template('contacts.html')
+
+@app.route("/reserver")
+def reserver():
+	return render_template('reserver.html')
+
+@app.route("/medias")
+def medias():
+	return render_template('medias.html')
+
+@app.route("/restaurant")
+def restaurant():
+	return render_template('restaurant.html')
+
+@app.route("/infos-pratiques")
+def infospratiques():
+	return render_template('infos-pratiques.html')
+
 
 if __name__ == '__main__':
     app.run(host = '127.0.0.1', port = 5000)
